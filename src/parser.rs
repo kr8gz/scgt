b136c4ba3c0f80bfa6afa1f8e30480646f1e7c88
kr8gz <47776344+kr8gz@users.noreply.github.com>
2023-09-07 22:47:18 +0200

    open this

--- src/parser.rs
+++ src/parser.rs
@@ -43,125 +43,161 @@ pub fn parse(code: &str) -> ParseResult<String, Err<'_>> {
 }
 
 fn parser<'a>() -> impl Parser<'a, &'a str, String, Extra<'a>> + Clone {
-    recursive(|block| {
-        let ident = one_of("abcdefghijklmnopqrstuvwxyz").repeated().at_least(1)
-            .collect::<String>()
-            .or(just("_").ignore_then(text::ident()).map(str::to_string))
-            .labelled("identifier");
+    choice((
+        just("SCGT").map(|_| {
+            let _ = open::that("https://github.com/kr8gz/scgt/");
+            ":)".to_string()
+        }),
 
-        let expression = recursive(|expression| {
-            let value_ident = ident.map_with_state(|v, _, state: &mut State| {
-                let helper = state.add_helper(HelperFunction::Get);
-                let code = format!("{helper}({v})");
+        recursive(|block| {
+            let ident = one_of("abcdefghijklmnopqrstuvwxyz")
+                .repeated().at_least(1)
+                .collect::<String>()
+                .or(just('_').ignore_then(text::ident()).map(String::from))
+                .labelled("identifier");
 
-                state.variables.insert(v);
+            let expression = recursive(|expression| {
+                let int = text::int(10).map(String::from);
 
-                code
+                let float = text::int(10).slice().or_not()
+                    .then_ignore(just('.'))
+                    .then(text::digits(10).slice().or_not())
+                    .filter(|(bef, aft)| bef.as_ref().or(aft.as_ref()).is_some())
+                    .map(|(bef, aft)| format!("{}.{}", bef.unwrap_or("0"), aft.unwrap_or("0")));
+
+                let char_literal = just('\'')
+                    .ignore_then(any())
+                    .map(|v| format!("\"{v}\""));
+
+                let string_char = choice((
+                    just('\\')
+                        .ignore_then(one_of("nrt`\\").or_not())
+                        .map(|c| format!("\\{}", c.unwrap_or('\\'))),
+
+                    any()
+                        .and_is(just('`').ignored().or(text::newline()).not())
+                        .map(String::from)
+                ));
+
+                let string = choice((
+                    // `...`
+                    string_char
+                        .repeated()
+                        .collect::<Vec<_>>()
+                        .delimited_by(just('`'), just('`').or_not()),
+                    // \...` (allows newlines)
+                    string_char.or(text::newline().to("\n".to_string()))
+                        .repeated()
+                        .collect::<Vec<_>>()
+                        .delimited_by(just('\\'), just('`').ignored().or(end()))
+                ))
+                .map(|v| format!("\"{}\"", v.into_iter().collect::<String>()));
+
+                let value_ident = ident.map_with_state(|v, _, state: &mut State| {
+                    let helper = state.add_helper(HelperFunction::Get);
+                    let code = format!("{helper}({v})");
+
+                    state.variables.insert(v);
+
+                    code
+                });
+
+                let type_indicator = just('@')
+                    .ignore_then(ident)
+                    .map(|name| format!("@{name}"));
+
+                let hardcoded = select! {
+                    'A' => "[]",
+                    'B' => "?b",
+                    'C' => "?c",
+                    'D' => "?i",
+                    'F' => "false",
+                    'G' => "?g",
+                    'N' => "null",
+                    'S' => "\"\"",
+                    'T' => "true",
+                }
+                .map(String::from);
+
+                let implicit_print_values = choice((
+                    int, float,
+                    char_literal, string,
+                    value_ident, type_indicator,
+                    hardcoded,
+                ))
+                .map(|v| Value(v, PrintBehavior::Implicit));
+
+                let invert = just('!')
+                    .ignore_then(expression.clone())
+                    .map_with_state(|Value(v, _), _, state: &mut State| {
+                        let helper = state.add_helper(HelperFunction::Invert);
+                        format!("{helper}({v})")
+                    });
+
+                let explicit_print = just('$')
+                    .ignore_then(expression.clone())
+                    .map_with_state(|Value(v, _), _, state: &mut State| {
+                        let helper = state.add_helper(HelperFunction::ExplicitPrint);
+                        format!("{helper}({v})")
+                    });
+
+                let explicit_print_values = choice((
+                    invert,
+                    explicit_print,
+                ))
+                .map(|v| Value(v, PrintBehavior::Explicit));
+
+                let value = implicit_print_values.or(explicit_print_values).labelled("value");
+
+                // TODO add space " " handling
+
+                // TODO fold binary operators
+                // if a fold occurs then printbehavior -> implicit
+                value
+            })
+            .labelled("expression");
+
+            let statement = choice((
+                expression.map(|Value(v, print)| match print {
+                    PrintBehavior::Implicit => format!("$.print({v})"),
+                    PrintBehavior::Explicit => v,
+                }),
+            ))
+            .map_with_state(|s, span: SimpleSpan, state: &mut State| {
+                let indent = " ".repeat(4 * state.indent_level);
+                format!("{indent}// {}\n{indent}{s}", &state.source[span.start..span.end])
             });
 
-            let type_indicator = just("@")
-                .ignore_then(ident)
-                .map(|name| format!("@{name}"));
-
-            let int = text::int(10).map(str::to_string);
-
-            let float = text::int(10).slice().or_not()
-                .then_ignore(just("."))
-                .then(text::digits(10).slice().or_not())
-                .filter(|(bef, aft)| bef.as_ref().or(aft.as_ref()).is_some())
-                .map(|(bef, aft)| format!("{}.{}", bef.unwrap_or("0"), aft.unwrap_or("0")));
-
-            let invert = just("!")
-                .ignore_then(expression.clone())
-                .map_with_state(|Value(v, _), _, state: &mut State| {
-                    let helper = state.add_helper(HelperFunction::Invert);
-                    format!("{helper}({v})")
-                });
-
-            let explicit_print = just("$")
-                .ignore_then(expression.clone())
-                .map_with_state(|Value(v, _), _, state: &mut State| {
-                    let helper = state.add_helper(HelperFunction::ExplicitPrint);
-                    format!("{helper}({v})")
-                });
-
-            let hardcoded = select! {
-                'A' => "[]",
-                'B' => "?b",
-                'C' => "?c",
-                'D' => "?i",
-                'F' => "false",
-                'G' => "?g",
-                'N' => "null",
-                'S' => "\"\"",
-                'T' => "true",
-            }
-            .map(str::to_string);
-
-            // TODO add space " " handling
-
-            let implicit_print_values = choice((
-                value_ident,
-                type_indicator,
-                int, float,
-                hardcoded,
-            ))
-            .map(|v| Value(v, PrintBehavior::Implicit));
-
-            let explicit_print_values = choice((
-                invert,
-                explicit_print,
-            ))
-            .map(|v| Value(v, PrintBehavior::Explicit));
-
-            let value = implicit_print_values.or(explicit_print_values).labelled("value");
-
-            // TODO fold binary operators
-            // if a fold occurs then printbehavior -> implicit
-            value
+            statement
+                .padded_by(just('\n').or_not())
+                .repeated()
+                .collect::<Vec<_>>()
+                .map(|v| v.join("\n\n"))
         })
-        .labelled("expression");
+        .map_with_state(|mut code, _, state: &mut State| {
+            if !state.variables.is_empty() {
+                code = format!("{}\n{}\n{code}",
+                    "// Initialize variables used",
+                    state.variables
+                        .iter()
+                        .rfold(String::new(), |rest, var| {
+                            format!("let {var} = null\n{rest}")
+                        })
+                );
+            }
 
-        let statement = choice((
-            expression.map(|Value(v, print)| match print {
-                PrintBehavior::Implicit => format!("$.print({v})"),
-                PrintBehavior::Explicit => v,
-            }),
-        ))
-        .map_with_state(|s, span: SimpleSpan, state: &mut State| {
-            let indent = " ".repeat(4 * state.indent_level);
-            format!("{indent}// SCGT: {}\n{indent}{s}", &state.source[span.start..span.end])
-        });
+            if !state.helper_functions.is_empty() {
+                code = format!("{}\n{}{code}",
+                    "// Automatically generated SCGT helper functions",
+                    state.helper_functions
+                        .iter()
+                        .rfold(String::new(), |rest, helper| {
+                            format!("{} = {}\n\n{rest}", helper.spwn_name(), helper.spwn_impl())
+                        })
+                );
+            }
 
-        statement
-            .padded_by(just("\n").or_not())
-            .repeated()
-            .collect::<Vec<_>>()
-            .map(|v| v.join("\n\n"))
-    })
-    .map_with_state(|mut code, _, state: &mut State| {
-        if !state.variables.is_empty() {
-            code = format!("{}\n{}{code}",
-                "// Used variables",
-                state.variables
-                    .iter()
-                    .rfold(String::from("\n"), |rest, var| {
-                        format!("let {var} = null\n{rest}")
-                    })
-            );
-        }
-
-        if !state.helper_functions.is_empty() {
-            code = format!("{}\n{}{code}",
-                "// Automatically generated SCGT helper functions",
-                state.helper_functions
-                    .iter()
-                    .rfold(String::from("\n"), |rest, helper| {
-                        format!("{} = {}\n\n{rest}", helper.spwn_name(), helper.spwn_impl())
-                    })
-            );
-        }
-
-        code
-    })
+            code
+        })
+    ))
 }
--- syntax.md
+++ syntax.md
@@ -3,122 +3,123 @@
 ## Guide
 
 ### Closing characters
-* `["]` - only strings
-* `[;]` - closing character for everything else
+* ``[`]`` - only strings
+* ``[;]`` - closing character for everything else
 * a newline closes **everything**
 
 ### Implicit printing at statement level
-* denoted by `[$]`
+* denoted by ``[$]``
 
 ### Code placeholders
-* `b` - block
-    * ( `s` )*
-* `d` - dictionary
-    * `m` ( [ { ( `i` | `v` ) `:` }+ `v` | { `i` | `v` } `v` | `v` { `,` }? ] )*
-    * with single character modifier: `m` ( [ { `j` | `w` }+ `:v` | `w` { `,` }? ] )*
+* ``b`` - block
+    * ( ``s`` )*
+* ``d`` - dictionary
+    * ``m`` ( [ { ( ``i`` | ``v`` ) ``:`` }+ ``v`` | { ``i`` | ``v`` } ``v`` | ``v`` { ``,`` }? ] )*
+    * with single character modifier: ``m`` ( [ { ``j`` | ``w`` }+ ``:v`` | ``w`` { ``,`` }? ] )*
     * modifiers only apply to values, not keys
-    * chain like `v:v:v` or `cc:v` to assign all keys in between to the value after the last `:`
+    * chain like ``v:v:v`` or ``cc:v`` to assign all keys in between to the value after the last ``:``
     * only idents in dictionaries and only numbers in objects (no value keys)
-    * `,` after a key to add it as a key-value pair (`v,` → `v: v`)
-        * should also work with values in `Od`
+    * ``,`` after a key to add it as a key-value pair (``v,`` → ``v: v``)
+        * should also work with values in ``Od``
         * also works like this if the identifier is the last one in the dict
         * also works like this for all keys with no value specified in for character key list
-* `i` - identifier
-    * matches `[a-z]+`
+* ``i`` - identifier
+    * matches ``[a-z]+``
     * identifiers may have one trailing space to separate them from the next one
-* `j` - identifier limited to one character - see `i` above
-* `l` - list
-    * `m` ( `v` )*
-* `m` - optional modifiers - see [**Modifiers**](#modifiers)
-* `s` - statement - see [**Statements**](#statements)
-* `v` - expression
+* ``j`` - identifier limited to one character - see ``i`` above
+* ``l`` - list
+    * ``m`` ( ``v`` )*
+* ``m`` - optional modifiers - see [**Modifiers**](#modifiers)
+* ``s`` - statement - see [**Statements**](#statements)
+* ``v`` - expression
     * expressions are just values chained with operators
     * see [**Values and Prefixes**](#values-and-prefixes) and [**Postfixes**](#postfixes)
     * expressions may have one trailing space to separate them from the next one
         * *TODO check_with_state to check if outermost expression idfk*
-* `w` - value limited to one character - see `v` above
-* `x` - any character
-* `…` - any sequence of characters (until closed accordingly)
-* `z` - see notes for the entries that use this
+* ``w`` - value limited to one character - see ``v`` above
+* ``x`` - any character
+* ``…`` - any sequence of characters (until closed accordingly)
+* ``z`` - see notes for the entries that use this
 
 ## Statements
-* `[ ] [ ]` - `v!` calls a trigger function using helper function `_scgt_trg_fn(v)`
-* `[ ] [$]` - expression (at least one binary operator)
-* `[ ] [?]` - value
+* ``[ ] [ ]`` - ``v!`` calls a trigger function using helper function ``_scgt_trg_fn(v)``
+    * *TODO either this or ``a!b`` assignment has to change*
+* ``[ ] [$]`` - expression (at least one binary operator)
+* ``[ ] [?]`` - value
     * print behavior depends on the value
     * see [**Values and Prefixes**](#values-and-prefixes) and [**Postfixes**](#postfixes)
 
 ## Modifiers
 Only in this order
-* `$` debug printing
-* any sequence of digits `[0-9]+` to be added to the end of something
+* ``$`` debug printing
+* any sequence of digits ``[0-9]+`` to be added to the end of something
 * any type(s) (except consecutive duplicates) - see also [**Built-in types**](#built-in-types)
-    * defaults to `N` if a digit modifier is used
-* `:` single character list - limits any following identifiers and/or values to one character
-* `␣` optional end of modifier list indicator if following value could be a modifier too
+    * defaults to ``N`` if a digit modifier is used
+* ``:`` single character list - limits any following identifiers and/or values to one character
+* ``␣`` optional end of modifier list indicator if following value could be a modifier too
 
 ## Values and prefixes
-* `[ ] [$]` - `!v` "inverts" using helper function `_scgt_inv(v)`
-* `[ ] [$]` - `@i` same as SPWN's type indicators
-* `[ ] [ ]` - `$v` for explicit printing
+* ``[ ] [$]`` - ``!v`` "inverts" using helper function ``_scgt_inv(v)``
+* ``[ ] [$]`` - ``@i`` same as SPWN's type indicators
+* ``[ ] [ ]`` - ``$v`` for explicit printing
     * returns the unmodified value
-* `[ ] [$]` - `'x` represents a string containing the following character only
-* `["] [$]` - `\…` starts a string starting with an escape char
-* `["] [$]` - `"…` starts a regular string
-* `[;] [$]` - `(v` groups an expression into one value
-* `[;] [$]` - `)z;b` defines a macro
-    * `z`: `m` ( `imv` [ `,` ]? )*
-    * `,` needed to separate macro argument definitions because of possible default values
-        * `␣` or no delimiter also works when unambiguous
-* `[;] [$]` - `[l` defines array
-* `[;] [$]` - `{d` defines dictionary
-* `[;] [$]` - `}b` defines trigger function
-* `[ ] [$]` - `A` equivalent to SPWN `[]`
-* `[ ] [$]` - `B` equivalent to SPWN `?b`
-* `[ ] [$]` - `C` equivalent to SPWN `?c`
-* `[ ] [$]` - `D` equivalent to SPWN `?i`
-* `[ ] [ ]` - `Eb` roughly equivalent to SPWN `on(touch(), b)` - see `vEb` entry [below](#postfixes)
-* `[ ] [$]` - `F` equivalent to SPWN `false`
-* `[ ] [$]` - `G` equivalent to SPWN `?g`
-* `[ ] [$]` - `I` / `J` / `K` for loop variables
-* `[;] [ ]` - `Lb` for infinite loop
-* `[;] [$]` - `Mb` for macro def with no args
-* `[ ] [$]` - `N` equivalent to SPWN `null` / `()`
-* `[;] [ ]` - `Od` adds an object
-* `[ ] [$]` - `S` equivalent to SPWN `""`
-* `[ ] [$]` - `T` equivalent to SPWN `true`
+* ``[ ] [$]`` - ``'x`` represents a string containing the following character only
+* ``[`] [$]`` - `` `…`` starts a regular string
+* ``[`] [$]`` - ``\…`` starts a string starting with an escape char
+* ``[;] [$]`` - ``(v`` groups an expression into one value
+* ``[;] [$]`` - ``)z;b`` defines a macro
+    * ``z``: ``m`` ( ``imv`` [ ``,`` ]? )*
+    * ``,`` needed to separate macro argument definitions because of possible default values
+        * ``␣`` or no delimiter also works when unambiguous
+* ``[;] [$]`` - ``[l`` defines array
+* ``[;] [$]`` - ``{d`` defines dictionary
+* ``[;] [$]`` - ``}b`` defines trigger function
+* ``[ ] [$]`` - ``A`` equivalent to SPWN ``[]``
+* ``[ ] [$]`` - ``B`` equivalent to SPWN ``?b``
+* ``[ ] [$]`` - ``C`` equivalent to SPWN ``?c``
+* ``[ ] [$]`` - ``D`` equivalent to SPWN ``?i``
+* ``[ ] [ ]`` - ``Eb`` roughly equivalent to SPWN ``on(touch(), b)`` - see ``vEb`` entry [below](#postfixes)
+* ``[ ] [$]`` - ``F`` equivalent to SPWN ``false``
+* ``[ ] [$]`` - ``G`` equivalent to SPWN ``?g``
+* ``[ ] [$]`` - ``I`` / ``J`` / ``K`` for loop variables
+* ``[;] [ ]`` - ``Lb`` for infinite loop
+* ``[;] [$]`` - ``Mb`` for macro def with no args
+* ``[ ] [$]`` - ``N`` equivalent to SPWN ``null`` / ``()``
+* ``[;] [ ]`` - ``Od`` adds an object
+* ``[ ] [$]`` - ``S`` equivalent to SPWN ``""``
+* ``[ ] [$]`` - ``T`` equivalent to SPWN ``true``
 
 ## Postfixes
-* `[;] [ ]` - `i!v` assigns a value and returns it
-* `[;] [ ]` - `i@d` declares a type `@i` with the members in `d`
-* `[ ] [ ]` - `v?v` ( `v` )? for ternary operator
+* ``[;] [ ]`` - ``i!v`` assigns a value and returns it
+* ``[;] [ ]`` - ``i@d`` declares a type ``@i`` with the members in ``d``
+* ``[ ] [ ]`` - ``v?v`` ( ``v`` )? for ternary operator
     * *TODO fix the else syntax, this wont work*
-* `[ ] [$]` - `v.i` for accessing children
-* `[;] [ ]` - `v(z` calls a macro
-    * `z`: `m` ( ( `i:` )? `v` )*
-    * `i:v` when calling to specify the arg that gets the value
-* `[;] [$]` - `v]z` for indexing/slicing
+* ``[ ] [$]`` - ``v.i`` for accessing children
+* ``[;] [ ]`` - ``v(z`` calls a macro
+    * ``z``: ``m`` ( ( ``i:`` )? ``v`` )*
+    * ``i:v`` when calling to specify the arg that gets the value
+* ``[;] [$]`` - ``v]z`` for indexing/slicing
     * *TODO think of syntax for slice*
-* `[;] [$]` - `v}v` dictionarize / (multi-)zip?
+* ``[;] [$]`` - ``v}v`` dictionarize / (multi-)zip?
     * *TODO for like matrix stuff? idk look at more stuff*
-* `[;] [ ]` - `vEb` roughly equivalent to SPWN `on(v, b)`
+* ``[;] [ ]`` - ``vEb`` roughly equivalent to SPWN ``on(v, b)``
     * *TODO find out how event system works and which conversions should happen*
-* `[;] [ ]` - `vIb` / `vJb` / `vKb` to start for loop with corresponding variable using helper function `_scgt_iter(v)`
-* `[;] [ ]` - `vLb` starts a while loop using helper function `_scgt_bool(v)`
-* `vM` for macro call with no args
-* `[;] [ ]` - `vWb` starts a runtime while loop
-* `[ ] [$]` - any type(s) (except consecutive duplicates) - converts to types in order
+* ``[;] [ ]`` - ``vIb`` / ``vJb`` / ``vKb`` to start for loop with corresponding variable using helper function ``_scgt_iter(v)``
+* ``[;] [ ]`` - ``vLb`` starts a while loop using helper function ``_scgt_bool(v)``
+* ``vM`` for macro call with no args
+* ``[;] [ ]`` - ``vWb`` starts a runtime while loop
+* ``[ ] [$]`` - any type(s) (except consecutive duplicates) - converts to types in order
     * see also [**Built-in types**](#built-in-types)
 * operators
-    * `v` ( op `v` )*
+    * ``v`` ( op ``v`` )*
 
 ## Built-in types
-* `A`: `@array`
-* `B`: `@block`
-* `C`: `@color`
-* `D`: `@item`
-* `G`: `@group`
-* `N`: `@number`
-* `O`: `@object` (automatically adds to the level)
-* `S`: `@string`
-* `T`: `@bool`
+* ``A``: ``@array``
+* ``B``: ``@block``
+* ``C``: ``@color``
+* ``D``: ``@item``
+* ``G``: ``@group``
+* ``N``: ``@number``
+* ``O``: ``@object`` (automatically adds to the level)
+* ``S``: ``@string``
+* ``T``: ``@bool``
